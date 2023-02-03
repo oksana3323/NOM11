74000788027d8580b4ec206512565a6ea5f97aeb
oksana <oksana3323@gmail.com>
2023-02-03 11:46:19 +0300

    HOM1.1

--- src/Account.java
+++ src/Account.java
@@ -2,24 +2,7 @@ public class Account {
 double balance;
 String name;
 
-//String дом;
-//int цена;
-//int комната;
-//int этаж;
-//int квадратметр;
-//int тел;
-// void myMethod() {
-//    System.out.println("depozit: "+ balance+"\n"+"Рассрочка без%на 12 месяца: "+дом+"\n"+"Неделя скидок 15%:" +
-//            ""+цена+"\n"+"Комната: "+комната+"\n"+"12 этажный дом : "+этаж
-//    +"\n"+"кв.м: "+квадратметр+"\n"+"телефон номер: "+тел); }
-//    void myMethod1() {
-//
-//
-//         System.out.println("withdrawal: " + balance + "\n" + "Рассрочка без%на 12 месяца: " + дом + "\n" + "Неделя скидок 15%:" +
-//                 "" + цена + "\n" + "Комната: " + комната + "\n" + "12 этажный дом : " + этаж
-//                 + "\n" + "кв.м: " + квадратметр + "\n" + "телефон номер: " + тел);
-//
-//     }
+//S
     public void deposit(double price){
         this.balance+=price;
     }
